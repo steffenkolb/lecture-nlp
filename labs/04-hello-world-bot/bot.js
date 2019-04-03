7a5215c8a5c2180fba6c68a80cf34790efe6df1c
Steffen Kolb <steffen@17k.de>
2019-04-03 14:09:26 +0200

    Fixes

--- labs/04-hello-world-bot/bot.js
+++ labs/04-hello-world-bot/bot.js
@@ -3,6 +3,12 @@
 
 const { ActivityTypes } = require('botbuilder');
 
+/**
+ * We will use compromise NLP library for text-matching.
+ * http://compromise.cool
+ */
+const nlp = require('compromise');
+
 class MyBot {
     /**
      *
@@ -13,8 +19,15 @@ class MyBot {
         // Message activities may contain text, speech, interactive cards, and binary or unknown attachments.
         // see https://aka.ms/about-bot-activity-message to learn more about the message and other activity types
         if (turnContext.activity.type === ActivityTypes.Message) {
-            let userMessage = turnContext.activity.text;
-            await turnContext.sendActivity(`You said "${userMessage}"`);
+            var rawtext = turnContext.activity.text
+
+            // interpret usermessage with compromise for further use
+            var userMessage = nlp(rawtext);
+
+            if(userMessage.match("welcome")) {
+                await turnContext.sendActivity(`Hello you!`);
+            }
+            await turnContext.sendActivity(`You said "${rawtext}"`);
         } else {
             // Generic handler for all other activity types.
             await turnContext.sendActivity(`[${ turnContext.activity.type } event detected]`);
