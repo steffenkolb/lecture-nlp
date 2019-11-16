b37be776efb76c17cd76c5a664dbb4aac8f1a46f
Steffen Kolb <steffen@17k.de>
2019-11-16 14:09:01 +0100

    Welcome to WS2019

--- resources/nodered/startup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-docker run \
-    -d \
-    -it \
-    -p 1880:1880 \
-    --network iot \
-    --mount type=bind,source="$(pwd)"/data,target=/data \
-    --name nodered-nlp \
-    nodered/node-red-docker
